8b4f1c821c6e683b0ece852f3c321cc14fac9870
Washirican <danrod@uw.edu>
2019-08-08 23:23:08 -0700

    Lesson 7 Django BLog rev 2019-08-08 11:22 PM

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-
 from blogging.models import Post, Category
+
+# FIXME: Reference Django ModelAdmin documentation
+# @admin.register(Category)
+# class CategoryAdmin(admin.ModelAdmin):
+#     pass
+
 
 admin.site.register(Post)
 admin.site.register(Category)
-
